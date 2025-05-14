38b5fc2784bcb66be2569c9e0e80b3890487abee
Danil <Napalkov-danil@list.ru>
2025-05-14 18:53:54 +0300

    Создание базы данных

---
+++ Телефонный справочник с фотографиями/DataBaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Телефонный_справочник_с_фотографиями
+{
+    internal class DataBaseConnection
+    {
+    }
+}
